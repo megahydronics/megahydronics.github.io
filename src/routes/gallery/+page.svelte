6b550d304439b69738d30cd2daaf7a8593814956
Pedram Amani <amani.pedram@gmail.com>
2023-07-25 18:16:59 -0700

    gallery image zoom removed

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,16 +1,6 @@
 <script>
     import Page from '$lib/Page.svelte';
-    import { imageInfos, iconInfos } from '$lib/index.js';
-
-    let imageId = null;
-
-    function showImage(event) {
-        imageId = event.currentTarget.id;
-    }
-
-    function hideImage() {
-        imageId = null;
-    }
+    import { imageInfos } from '$lib/index.js';
 </script>
 
 <Page pageId="gallery">
@@ -20,33 +10,16 @@
             class="mt-12 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-8"
         >
             {#each Object.values(imageInfos) as info}
-                <button
-                    id={info.id}
-                    on:click={showImage}
-                    class="aspect-h-1 aspect-w-1 w-full overflow-hidden cursor-zoom-in hover:opacity-75 rounded-md lg:aspect-none lg:h-80"
+                <div
+                    class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80"
                 >
                     <img
                         src={info.src}
                         alt={info.alt}
                         class="h-full w-full object-cover object-center lg:h-full lg:w-full"
                     />
-                </button>
+                </div>
             {/each}
         </div>
     </div>
-
-    {#if imageId}
-        <button
-            class="fixed flex flex-col inset-0 bg-gray-500 bg-opacity-75 justify-center items-center cursor-zoom-out sm:p-8"
-            on:click={hideImage}
-        >
-            <div class="relative overflow-hidden sm:rounded-md">
-                <img
-                    src={imageInfos[imageId].src}
-                    alt={imageInfos[imageId].alt}
-                    class="object-center max-w-full max-h-full"
-                />
-            </div>
-        </button>
-    {/if}
 </Page>
